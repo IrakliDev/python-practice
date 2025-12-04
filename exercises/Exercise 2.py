996d9250aad29c2a0124ea5f77fed2ccfe5db9a8
Irakli Tsutskiridze <flamee0777@gmail.com>
2025-12-04 16:06:39 +0100

    Remove Exercise 1-5.py files from GitHub

--- exercises/Exercise 2.py
+++
@@ -1,9 +0,0 @@
-# Exercise Number 2 Shopping Cart
-
-item = input("What item would you like to buy?: ")
-priced = float(input("What is the price in dollars?: "))
-quantity1 = int(input("how many would you like to buy?: "))
-total = priced * quantity1
-
-print(f"You have bought {quantity1} of {item}/s")
-print(f"This will be {total}$ <3")
